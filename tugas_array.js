4bf71d1738f3b8e2823e4bb6dbd49b413837a03c
FIRST_NAME LAST_NAME <mulianaibrahim@gmail.com>
2023-03-09 10:28:13 +0700

    tugas array

---
+++ tugas_array.js
@@ -0,0 +1,26 @@
+//1. Buat method untuk menambahkan data ke dalam suatu array.
+console.log('Method untuk menambahkan data ke dalam array')
+
+let menuMakanan = ['nasi goreng', 'mie goreng', 'nasi padang', 'ayam geprek', 'baksoe'];
+console.log(menuMakanan)
+menuMakanan.push('ikan bakar', '2 teh jihau')
+console.log(menuMakanan)
+
+//2. Buat method untuk mengambil data dari dalam array berdasarkan index
+console.log('method untuk mengambil data dari dalam array berdasarkan index')
+console.log(menuMakanan[0]);
+console.log(menuMakanan[1]);
+console.log(menuMakanan[2]);
+console.log(menuMakanan[3]);
+console.log(menuMakanan[4]);
+console.log(menuMakanan[5]);
+console.log(menuMakanan[6]);
+
+
+//3. buat method untuk mengganti nilai dari sebuah index pada array
+console.log('method untuk mengganti nilai dari sebuah index pada array')
+menuMakanan[0] = 'tempe goreng'
+menuMakanan[1] = 'burger'
+menuMakanan[2] = 'kebab'
+console.log(menuMakanan)
+
